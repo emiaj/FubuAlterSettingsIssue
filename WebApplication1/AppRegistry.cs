955c85d8388f9ca12ace205aaa9a15d2ea4ff9b4
jfebres <jaime.febres@gmail.com>
2012-12-06 20:49:05 -0500

    Initial commit.

---
+++ WebApplication1/AppRegistry.cs
@@ -0,0 +1,13 @@
+﻿using FubuMVC.Core;
+using WebApplication1.Features.Home;
+
+namespace WebApplication1
+{
+    public class AppRegistry : FubuRegistry
+    {
+        public AppRegistry()
+        {
+            Routes.HomeIs<HomeEndpoint>(x => x.get(null));
+        }
+    }
+}
---
+++ WebApplication1/Features/Home/HomeEndpoint.cs
@@ -0,0 +1,19 @@
+﻿namespace WebApplication1.Features.Home
+{
+    public class HomeEndpoint
+    {
+        public HomeModel get(HomeInput input)
+        {
+            return new HomeModel();
+        }
+    }
+
+    public class HomeInput
+    {
+    }
+
+    public class HomeModel
+    {
+    }
+
+}
